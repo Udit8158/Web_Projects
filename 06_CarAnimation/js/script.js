58fe29b64ac9ed54fdbadfdc96d6fe8a21f94462
Udit8158 <kunduudit8158@gmail.com>
2021-10-05 13:52:53 +0530

    add 06 car animation first version

---
+++ 06_CarAnimation/js/script.js
@@ -0,0 +1,7 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src', 'sound.mp3'); 
+// audio.play();
+
+var audio = new Audio('../sound.mp3')
+// audio.play();
+audio.loop=true;
