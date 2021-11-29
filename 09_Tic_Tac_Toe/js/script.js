753b19b6b66b164524568d0d01b4127b20f985d9
Udit8158 <kunduudit8158059368@gmail.com>
2021-11-29 13:40:32 +0530

    working on tic tac toe game

--- 09_Tic_Tac_Toe/js/script.js
+++ 09_Tic_Tac_Toe/js/script.js
@@ -14,6 +14,32 @@ let turn = "X";
 
 //Hide the image gif initially
 imgbox.style.display = "none";
+
+//Check win
+let checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    //making array of win possibilities
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach( e => {
+        console.log(e[2]);
+        let boxtext = document.getElementsByClassName('boxtext');
+        console.log(boxtext[e[2]]);  //these lines are for feel
+        // console.log(boxtext);
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            let info = document.querySelector(".info");
+            info.innerText = `${boxtext[e[0]].innerText} Won`;
+        }
+    })
+}
 //Print the X or 0
 boxes.forEach((e)=>{
     e.addEventListener("click",(element)=>{
@@ -21,10 +47,16 @@ boxes.forEach((e)=>{
             element.target.innerText = "X";
             turn = "0";
             info.innerText = "Turn for 0";
+            tingMusic.play();
+            checkWin();
         }
         else {
             element.target.innerText = "0";
             turn = "X";
+            info.innerText = "Turn for X";
+            tingMusic.play();
+            checkWin()
         }
     })
 })
+
