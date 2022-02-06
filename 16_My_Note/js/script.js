a5fe994a1eb22fc293b10132a24814a7ffd372a7
Udit8158 <kunduudit8158@gmail.com>
2022-02-06 17:10:57 +0530

    add my new note application.It is in early stage working on it.Now it has lots of bugs

---
+++ 16_My_Note/js/script.js
@@ -0,0 +1,101 @@
+console.log("Welcome");
+
+// Grab elements
+const addNoteBtn = document.querySelector(".add-note-btn");
+const smallNoteViewContainer = document.querySelector(
+  ".small-note-view-container"
+);
+const previewContainer = document.querySelector(".preview-container");
+const previewNoteTitle = document.querySelector(".note-title");
+const previewNoteBody = document.querySelector(".note-body");
+
+//Declare important variables
+const notesData = localStorage.getItem("notesData");
+
+// Creating functions
+const showNote = () => {
+  if (notesData === null) {
+    notesArr = [];
+  } else {
+    notesArr = JSON.parse(notesData);
+  }
+  let html = "";
+  notesArr.forEach((element, index) => {
+    html += `
+      <div class="small-note-view" id="${index}">
+            <div class="small-note-title">${element.noteTitle}</div>
+            <div class="small-note-body">${element.noteBody}</div>
+            <div class="small-note-btns">
+              <div class="delete-btn" >
+                <i class="fa fa-trash-o" style="font-size: 24px"></i>
+              </div>
+              <div class="save-btn" onclick="saveNote()" >
+                <i class="fa fa-save" style="font-size: 24px"></i>
+              </div>
+            </div>
+            <div class="time-update">Time</div>
+          </div>
+      `;
+  });
+  if (notesArr.length != 0) {
+    Array.from(previewContainer.children).forEach((e) => {
+      e.classList.add("hide");
+    });
+    smallNoteViewContainer.innerHTML = html;
+  } else {
+    Array.from(previewContainer.children).forEach((e) => {
+      e.classList.add("hide");
+    });
+    smallNoteViewContainer.innerHTML = "You donot have notes to show.";
+  }
+};
+showNote(); // Call this function when the website is start
+
+const saveNote = () => {
+  console.log("clicked");
+  notesArr = JSON.parse(localStorage.getItem("notesData"));
+  let myObj = {
+    noteTitle: previewNoteTitle.value,
+    noteBody: previewNoteBody.value,
+  };
+  console.log(this);
+  notesArr.splice(0, 1, myObj);
+  localStorage.setItem("notesData", JSON.stringify(notesArr));
+  showNote();
+};
+
+// Event listners
+addNoteBtn.addEventListener("click", () => {
+  if (notesData === null) {
+    notesArr = [];
+  } else {
+    notesArr = JSON.parse(notesData);
+  }
+  let myObj = {
+    noteTitle: "I am note title",
+    noteBody: "I am note body",
+  };
+  notesArr.push(myObj);
+  localStorage.setItem("notesData", JSON.stringify(notesArr));
+  document.location.reload();
+  showNote();
+});
+
+// Active class adding and removing stuff
+Array.from(smallNoteViewContainer.children).forEach((smallNote) => {
+  smallNote.addEventListener("click", function () {
+    // Show the preview container after clicking the small note
+    Array.from(previewContainer.children).forEach((e) => {
+      e.classList.remove("hide");
+    });
+
+    // Method to remove active class from the unclicked small notes
+    let newFilteredArr = Array.from(smallNoteViewContainer.children).filter(
+      (e) => e.id != this.id
+    );
+    newFilteredArr.forEach((e) => e.classList.remove("active-note"));
+
+    // then add the active class in the clicked small note
+    this.classList.add("active-note");
+  });
+});
